f33560417a0e7261fdfeead04cc79868bf8aa15a
AnakinRibeiro <eduardor.contato@outlook.com>
2025-04-29 15:42:09 -0300

    Added checkbox storie

---
+++ src/components/ui/checkbox/stories.tsx
@@ -0,0 +1,36 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { fn } from "@storybook/test";
+import { Checkbox } from ".";
+
+export default {
+  title: "Components/Checkbox",
+  component: Checkbox,
+  tags: ["autodocs"],
+  args: {
+    label: "Remember Password",
+    onInputChange: fn(),
+    checked: false,
+  },
+  argTypes: {
+    label: {
+      control: "text",
+      description: "Checkbox label",
+    },
+    onInputChange: {
+      action: "inputChanged",
+      description: "Action triggered when input value is changed",
+    },
+    checked: {
+      control: "boolean",
+      description: "Defines if checkbox is checked or not",
+      table: {
+        type: { summary: "boolean" },
+        defaultValue: { summary: "false" },
+      },
+    },
+  },
+} satisfies Meta<typeof Checkbox>;
+
+type Story = StoryObj<typeof Checkbox>;
+
+export const Default: Story = {};
