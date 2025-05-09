855468f165a43d35b42fb8ec89e428ac017eef51
AnakinRibeiro <eduardor.contato@outlook.com>
2025-05-09 13:56:34 -0300

    Fixed table row font

--- src/components/ui/table/TableRow.tsx
+++ src/components/ui/table/TableRow.tsx
@@ -20,7 +20,10 @@ export function TableRow<T>({ row, columns, onClick }: Props<T>) {
           value.length > 32 ? `${value.slice(0, 32)}...` : value;
 
         return (
-          <td className="px-[20px] py-[10px] text-left border-b border-gray font-rubik text-[12.5px] text-black">
+          <td
+            className="px-[20px] py-[10px] text-left border-b border-gray font-rubik text-[12.5px] text-black"
+            key={i}
+          >
             {column.renderFirstCell
               ? column.renderFirstCell(row)
               : displayValue}
